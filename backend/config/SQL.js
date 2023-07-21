1dc6e9a24e9948ebbf96c5b700dd1ee93e29ec32
Rodaca <leapardo4@gmail.com>
2023-07-21 09:28:24 -0500

    feat: :memo: se escribe el select de creyentes

--- backend/config/SQL.js
+++ backend/config/SQL.js
@@ -17,3 +17,13 @@ const getConnection = () =>{
 };
 
 export default getConnection;
+/*  select Creyente.*,
+Barrio.nombreBarrio,Barrio.idComuna,
+Comuna.nombreComuna,Comuna.idMunicipio,
+Municipio.NombreMunicipio,Municipio.idDepartamento,
+Departamento.nombreDepartamento
+from Creyente,Barrio,Comuna,Municipio,Departamento 
+where Creyente.idBarrio = Barrio.idBarrio AND 
+Barrio.idComuna = Comuna.idComuna AND 
+Municipio.idMunicipio=Comuna.idMunicipio AND 
+Departamento.idDepartamento=Municipio.idDepartamento;   */
